6e90ba10f83d1b9b8a638cad2706e609b0d355d6
Szymon Lechowicz <szymon.piotr.lechowicz@gmail.com>
2021-03-17 12:35:15 +0100

    Lab. 1 na 5.5, jest w pytke

---
+++ rpq/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include "rpq.h"
+#include "schrage.h"
+
+using namespace std;
+
+int main()
+{
+    
+    string s;
+    int sum = 0;
+    int cmax;
+    
+    clock_t t;
+    t = clock();
+    ifstream file("data.txt");
+    for (int f = 1; f <= 4; f++)
+    {
+        while (s != "data." + to_string(f)) file >> s;
+
+        int n = 0;
+        file >> n;
+
+        QueueTask queue;
+        Task* task = new Task[n];
+        for (int i = 0; i < n; i++)
+        {
+            file >> task[i].r;
+            file >> task[i].p;
+            file >> task[i].q;
+            task[i].order = i + 1;
+            queue.push(task[i]);
+        }
+
+        cmax = schrage_prmt(queue, task);
+        cout << "Time" << f << ":\t" << cmax << endl;
+        sum += cmax;
+    }
+    t = clock() - t;
+    cout << "Total time:\t" << sum << endl;
+    cout << "Program time: \t" << ((float)t) / CLOCKS_PER_SEC << " s" << endl;
+
+
+}
---
+++ rpq/schrage.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "rpq.h"
+
+int schrage(QueueTask qn, Task* tasks)
+{
+    int t = 0;
+    int i = 0;
+    int cmax = 0;
+    ReadyQueueTask ready;
+    Task e;
+    while (!qn.empty() || !ready.empty())
+    {
+        while (!qn.empty() && (qn.top().r <= t))
+        {
+            e = qn.top();
+            qn.pop();
+            ready.push(e);
+        }
+        if (ready.empty())
+        {
+            t = qn.top().r;
+        }
+        else
+        {
+            e = ready.top();
+            ready.pop();
+            tasks[i] = e;
+            i++;
+            t += e.p;
+            cmax = max(cmax, t + e.q);
+        }
+    }
+    return cmax;
+}
+
+int schrage_prmt(QueueTask qn, Task* tasks)
+{
+    int t = 0;
+    int i = 0;
+    int cmax = 0;
+    ReadyQueueTask ready;
+    Task e;
+    Task l;
+    l.q = INT_MAX;
+    l.p = 0;
+    l.r = 0;
+
+    while (!qn.empty() || !ready.empty())
+    {
+        while (!qn.empty() && (qn.top().r <= t))
+        {
+            e = qn.top();
+            qn.pop();
+            ready.push(e);
+            if (e.q > l.q)
+            {
+                l.p = t - e.r;
+                t = e.r;
+                if (l.p > 0)
+                {
+                    ready.push(l);
+                }
+            }
+        }
+        if (ready.empty())
+        {
+            t = qn.top().r;
+        }
+        else
+        {
+            e = ready.top();
+            ready.pop();
+            l = e;
+            t += e.p;
+            cmax = max(cmax, t + e.q);
+        }
+    }
+    return cmax;
+}
+
